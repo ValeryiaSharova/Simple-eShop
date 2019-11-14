5cd872d0ede7b7bc31ed5780a29a2a42222d1734
Aller-Forst <gominzyplay@gmail.com>
2019-11-14 15:37:24 +0300

    Implemented admin page filling by using JS.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+"use strict"
+
+let adminPageData = (function() {
+    const tableOfUsers = [["Anastasia", "Novikova", "Nov@mail.ru", null],
+                        ["Valeryia", "Nemankova", "Neman@gmail.com", null],
+                        ["Igor", "Leonov", "Xan@yandex.com", null],
+                        ["Pavel", "Zinkevich", "Pavel@gmail.com", "Remove"]];
+
+    tableOfUsers.forEach(function(user, index) {
+        
+        const tableRow = document.createElement("tr");
+        const tableHeader = document.createElement("th");
+        tableHeader.textContent = index + 1;
+        tableRow.append(tableHeader);
+        
+        user.forEach(function(userData){
+            const tableData = document.createElement("td");
+            tableData.textContent = userData;
+            tableRow.append(tableData);
+        });
+        
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("btn", "btn-color");
+        removeButton.setAttribute("type", "button");
+        removeButton.textContent = "Remove";
+        const lastTableRow = document.createElement("td");
+        lastTableRow.append(removeButton);
+        tableRow.append(lastTableRow);
+        const tbody = document.querySelector("#tableOfUsers > tbody");
+        
+        tbody.append(tableRow);
+    });
+})();
