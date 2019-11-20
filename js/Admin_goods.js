4f1690878937dfa96760075a065822dcba859be8
Aller-Forst <gominzyplay@gmail.com>
2019-11-21 02:00:02 +0300

    Implemented an opportunity for admin to delete users, add and delete goods.

--- js/Admin_goods.js
+++ js/Admin_goods.js
@@ -39,59 +39,109 @@ let adminGoodsData = (function() {
                         }
                     ];
 
-    goodsData.forEach(function(good) {
-        const card = document.createElement("div");
-        card.classList.add("card", "box-shadow");
-        
-        const image = document.createElement("img");
-        image.classList.add("card-img-top");
-        image.setAttribute("src", good.picture);
-        image.setAttribute("alt", good.title);
-        card.append(image);
+    function renderGoodsData() {
+            goodsData.forEach(function(good) {
+            const card = document.createElement("div");
+            card.classList.add("card", "box-shadow");
+            
+            const image = document.createElement("img");
+            image.classList.add("card-img-top");
+            image.setAttribute("src", good.picture);
+            image.setAttribute("alt", good.title);
+            card.append(image);
 
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
+            const cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
 
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title", "text-center");
-        cardTitle.textContent = good.title;
-        cardBody.append(cardTitle);
+            const cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title", "text-center");
+            cardTitle.textContent = good.title;
+            cardBody.append(cardTitle);
 
-        const cardText = document.createElement("p");
-        cardText.classList.add("card-text", "text-justify");
-        cardText.textContent = good.description;
-        cardBody.append(cardText);
+            const cardText = document.createElement("p");
+            cardText.classList.add("card-text", "text-justify");
+            cardText.textContent = good.description;
+            cardBody.append(cardText);
 
-        const cardTag = document.createElement("p");
-        cardTag.classList.add("card-text");
-        const small = document.createElement("small");
-        small.classList.add("text-muted");
-        small.textContent = good.tags;
-        cardTag.append(small);
-        cardBody.append(cardTag);
+            const cardTag = document.createElement("p");
+            cardTag.classList.add("card-text");
+            const small = document.createElement("small");
+            small.classList.add("text-muted");
+            small.textContent = good.tags;
+            cardTag.append(small);
+            cardBody.append(cardTag);
 
-        const cardButton = document.createElement("div");
-        cardButton.classList.add("text-center");
-        const buyButton = document.createElement("a");
-        buyButton.classList.add("btn", "btn-color", "btn-rounded", "my-1", "mx-1");
-        buyButton.setAttribute("href", "#");
-        buyButton.textContent = `Buy for ${good.price}$`;
-        const buttonImg = document.createElement("i");
-        buttonImg.classList.add("fas", "fa-angle-right", "rounded-circle", "ml-1", "style-circle");
-        buyButton.append(buttonImg);
-        cardButton.append(buyButton);
-        const deleteButton = document.createElement("a");
-        deleteButton.classList.add("btn", "btn-color", "btn-rounded", "my-1", "mx-1");
-        deleteButton.setAttribute("href", "#");
-        deleteButton.textContent = "Delete";
-        const deleteImg = document.createElement("i");
-        deleteImg.classList.add("fas", "fa-times", "rounded-circle", "ml-1", "style-circle");
-        deleteButton.append(deleteImg);
-        cardButton.append(deleteButton);
-        cardBody.append(cardButton);
-        card.append(cardBody);
+            const cardButton = document.createElement("div");
+            cardButton.classList.add("text-center");
+            const buyButton = document.createElement("a");
+            buyButton.classList.add("btn", "btn-color", "btn-rounded", "my-1", "mx-1");
+            buyButton.setAttribute("href", "#");
+            buyButton.textContent = `Buy for ${good.price}$`;
+            const buttonImg = document.createElement("i");
+            buttonImg.classList.add("fas", "fa-angle-right", "rounded-circle", "ml-1", "style-circle");
+            buyButton.append(buttonImg);
+            cardButton.append(buyButton);
+            const deleteButton = document.createElement("button");
+            deleteButton.classList.add("btn", "btn-color", "btn-rounded", "my-1", "mx-1");
+            deleteButton.setAttribute("href", "#");
+            deleteButton.textContent = "Delete";
+            deleteButton.onclick = goodRemoveButtonHandler;
+            const deleteImg = document.createElement("i");
+            deleteImg.classList.add("fas", "fa-times", "rounded-circle", "ml-1", "style-circle");
+            deleteButton.append(deleteImg);
+            cardButton.append(deleteButton);
+            cardBody.append(cardButton);
+            card.append(cardBody);
 
-        const cardColumns = document.querySelector(".card-columns");
-        cardColumns.prepend(card);
-    })
+            const cardColumns = document.querySelector(".card-columns");
+            cardColumns.prepend(card);
+        });
+    }
+
+    function cleanGoodsData() {
+        $("#add-good").prevUntil().remove();
+    }
+
+    function cleanForm() {
+        $("#title").val("");
+        $("#description").val("");
+        $("#price").val("");
+        $("#picture").val("");
+        $("#tags").val("");
+    }
+
+    function goodRemoveButtonHandler(event) {
+        event.preventDefault();
+        const button = $(this);
+        const currentDiv = button.parents(".card-body");
+        const currentGoodTitle = currentDiv.children(".card-title").text();
+        let position = 0;
+        for (let i = 0; i < goodsData.length; i++) {
+            if (currentGoodTitle === goodsData[i].title) {
+                position = i;
+                break;
+            }
+        }
+        goodsData.splice(position, 1);
+        cleanGoodsData();
+        renderGoodsData();
+    }
+    
+    renderGoodsData();
+
+    $("#add-good-form").submit(function(event) {
+        event.preventDefault();
+        const newUser = {
+            title: $("#title").val(),
+            description: $("#description").val(),
+            price: +$("#price").val(),
+            picture: $("#picture").val(),
+            tags: $("#tags").val()
+        }
+        $("#add-good-modal").modal("hide");
+        goodsData.unshift(newUser);
+        cleanForm();
+        cleanGoodsData();
+        renderGoodsData();
+    });
 })();
--- js/Admin_page.js
+++ js/Admin_page.js
@@ -39,29 +39,64 @@ let adminPageData = (function() {
                             }
                         ];
 
-    tableOfUsers.forEach(function(user, index) {
-        
-        const tableRow = document.createElement("tr");
-        const tableHeader = document.createElement("th");
-        tableHeader.textContent = index + 1;
-        tableRow.append(tableHeader);
-        
-        for (let key in user) {
-            const tableData = document.createElement("td");
-            tableData.textContent = user[key];
-            tableRow.append(tableData);
-        }
-           
-        const removeButton = document.createElement("button");
-        removeButton.setAttribute("id", "delete");
-        removeButton.classList.add("btn", "btn-color");
-        removeButton.setAttribute("type", "button");
-        removeButton.textContent = "Remove";
-        const lastTableRow = document.createElement("td");
-        lastTableRow.append(removeButton);
-        tableRow.append(lastTableRow);
+    function renderTableOfUsers() {
+            tableOfUsers.forEach(function(user, index) {
+            
+            const tableRow = document.createElement("tr");
+            const tableHeader = document.createElement("th");
+            tableHeader.textContent = index + 1;
+            tableRow.append(tableHeader);
+            
+            for (let key in user) {
+                const tableData = document.createElement("td");
+                if (key === "mail") {
+                    tableData.setAttribute("name", "user-mail");
+                }
+                tableData.textContent = user[key];
+                tableRow.append(tableData);
+            }
+            
+            const removeButton = document.createElement("button");
+            removeButton.classList.add("btn", "btn-color");
+            removeButton.setAttribute("type", "button");
+            removeButton.setAttribute("name", "user-remove-button");
+            removeButton.onclick = userRemoveButtonHandler;
+            removeButton.textContent = "Remove";
+            const lastTableRow = document.createElement("td");
+            lastTableRow.append(removeButton);
+            tableRow.append(lastTableRow);
+
+            const tbody = document.querySelector("#table-of-users > tbody");
+            tbody.append(tableRow);
+        });
+    }
+
+    function cleanUserTable() {
         const tbody = document.querySelector("#table-of-users > tbody");
-        
-        tbody.append(tableRow);
-    });
+        tbody.innerHTML = "";
+    }
+
+    function userRemoveButtonHandler(event) {
+        event.preventDefault();
+        const button = $(this);
+        const currentTr = button.parents("tr");
+        const currentUserMail = currentTr.children("td[name=user-mail]").text();
+        let position = -1;
+        for (let i = 0; i < tableOfUsers.length; i++) {
+            if (currentUserMail === tableOfUsers[i].mail && tableOfUsers[i].deleteRequest) {
+                position = i;
+                break;
+            }
+        }
+        if (position === -1) {
+            alert("You can't delete this user");
+        } else {
+            tableOfUsers.splice(position, 1);
+            cleanUserTable();
+            renderTableOfUsers();
+        }
+    }
+
+    renderTableOfUsers();
+
 })();
