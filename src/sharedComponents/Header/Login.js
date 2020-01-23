06f86a35981d187e634776bcff3cd0cee75eccf5
Aller-Forst <gominzyplay@gmail.com>
2020-01-23 22:17:38 +0300

    Some changes in Login component.

--- src/sharedComponents/Header/Login.js
+++ src/sharedComponents/Header/Login.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => (
-  <div className="tab-pane active" id="login" role="tabpanel">
-    <div className="modal-body">
-      <form id="login-user">
-        <div className="form-group">
-          <label htmlFor="input-email">E-mail:</label>
-          <input type="email" className="form-control" id="input-login-email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="input-pass">Password:</label>
-          <input type="password" className="form-control" id="input-login-pass" />
-        </div>
-        <div className="text-center mt-2">
-          <input type="submit" value="Login" className="btn btn-modal" />
-        </div>
-      </form>
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+
+  return (
+    <div className="tab-pane active" id="login" role="tabpanel">
+      <div className="modal-body">
+        <form id="login-user">
+          <div className="form-group">
+            <label htmlFor="input-email">E-mail:</label>
+            <input
+              type="email"
+              className="form-control"
+              id="input-login-email"
+              onChange={e => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-pass">Password:</label>
+            <input
+              type="password"
+              className="form-control"
+              id="input-login-pass"
+              onChange={e => {
+                setPass(e.target.value);
+              }}
+            />
+          </div>
+          <div className="text-center mt-2">
+            <input type="submit" value="Login" className="btn btn-modal" />
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
