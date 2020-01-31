d6d8652aaae50038b54b0a107941e8370863a843
Aller-Forst <gominzyplay@gmail.com>
2020-01-31 21:24:53 +0300

    Added the constants for actions.

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,2 +1,6 @@
 export const DELETE_GOOD = 'DELETE_GOOD';
 export const ADD_GOOD = 'ADD_GOOD';
+export const DELETE_USER = 'DELETE_USER';
+export const ADD_USER = 'ADD_USER';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
