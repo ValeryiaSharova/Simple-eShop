422bd9e9839b61d8313607f6f7fbf5e938c7b263
Aller-Forst <gominzyplay@gmail.com>
2020-02-18 16:36:02 +0300

    Code refactoring: form validation was improved.

--- src/pages/Admin/components/DialogAddGood.js
+++ src/pages/Admin/components/DialogAddGood.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -6,80 +6,35 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'proptypes';
 import * as yup from 'yup';
+import { Formik, Form, Field } from 'formik';
 
 const AddGood = ({ onRequestClose, addGood }) => {
-  const [goodNew, setGoodNew] = useState({
-    id: 0,
-    title: '',
-    description: '',
-    price: 0,
-    picture: '',
-    tags: '',
-  });
-
-  const [error, setError] = useState({ title: '', price: '', picture: '', tags: '' });
-
-  const validUrlRegex = RegExp(
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
-  );
-
   const schema = yup.object().shape({
     title: yup
       .string()
-      .min(5)
-      .required(),
+      .trim()
+      .min(5, 'Title must be longer')
+      .required('Required'),
+    description: yup.string(),
     price: yup
       .number()
-      .positive()
-      .required(),
+      .positive('Price must be positive')
+      .required('Required'),
     picture: yup
       .string()
-      .url()
-      .required(),
+      .url('Picture must be url')
+      .required('Required'),
     tags: yup
       .string()
-      .min(4)
-      .required(),
+      .trim()
+      .min(4, 'Tags must be longer')
+      .required('Required'),
   });
 
-  const handleAddGood = e => {
-    let { value } = e.target;
-    const { name } = e.target;
-    switch (name) {
-      case 'title': {
-        error.title = value.length < 5 ? 'Title must be longer' : '';
-        break;
-      }
-      case 'price': {
-        value = Number(value);
-        error.price = value < 0 ? 'Price must be positive' : '';
-        break;
-      }
-      case 'picture': {
-        error.picture = validUrlRegex.test(value) ? '' : 'Url is not valid';
-        break;
-      }
-      case 'tags': {
-        error.tags = value.length < 4 ? 'Tags must be longer' : '';
-        break;
-      }
-      default:
-        break;
-    }
-    setError({ ...error });
-    setGoodNew({ ...goodNew, [name]: value });
-  };
-
-  const add = e => {
-    e.preventDefault();
-    schema.isValid(goodNew).then(valid => {
-      if (valid) {
-        addGood(goodNew);
-        onRequestClose();
-      } else {
-        alert('Invalid form!');
-      }
-    });
+  const add = values => {
+    const goodNew = { id: 0, ...values };
+    addGood(goodNew);
+    onRequestClose();
   };
 
   return (
@@ -87,86 +42,68 @@ const AddGood = ({ onRequestClose, addGood }) => {
       <DialogTitle aria-labelledby="customized-dialog-title">Add a new game</DialogTitle>
       <DialogContent dividers>
         <div className="modal-body">
-          <form id="add-good-form" noValidate="novalidate">
-            <fieldset>
-              <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  id="title"
-                  name="title"
-                  required=""
-                  aria-required="true"
-                  onChange={handleAddGood}
-                />
-                {error.title.length > 0 ? (
-                  <span className="error text-center">{error.title}</span>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <textarea
-                  className="form-control"
-                  id="description"
-                  name="description"
-                  required=""
-                  aria-required="true"
-                  onChange={handleAddGood}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="price">Price</label>
-                <input
-                  className="form-control"
-                  type="number"
-                  id="price"
-                  name="price"
-                  min="0"
-                  required=""
-                  aria-required="true"
-                  onChange={handleAddGood}
-                />
-                {error.price.length > 0 ? (
-                  <span className="error text-center">{error.price}</span>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <label htmlFor="picture">Picture</label>
-                <input
-                  className="form-control"
-                  type="url"
-                  id="picture"
-                  name="picture"
-                  required=""
-                  aria-required="true"
-                  onChange={handleAddGood}
-                />
-                {error.picture.length > 0 ? (
-                  <span className="error text-center">{error.picture}</span>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <label htmlFor="tags">Tags</label>
-                <textarea
-                  className="form-control"
-                  id="tags"
-                  name="tags"
-                  required=""
-                  aria-required="true"
-                  onChange={handleAddGood}
-                />
-                {error.tags.length > 0 ? (
-                  <span className="error text-center">{error.tags}</span>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <button className="btn btn-modal" onClick={add} type="button">
-                  Create
-                </button>
-              </div>
-            </fieldset>
-          </form>
+          <Formik
+            initialValues={{ title: '', description: '', price: 0, picture: '', tags: '' }}
+            validationSchema={schema}
+            onSubmit={add}
+          >
+            {({ values, errors, touched }) => (
+              <Form>
+                <fieldset>
+                  <div className="form-group">
+                    <label htmlFor="title">Title</label>
+                    <Field className="form-control" type="text" name="title" value={values.title} />
+                    {errors.title && touched.title ? (
+                      <span className="error text-center">{errors.title}</span>
+                    ) : null}
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <Field
+                      className="form-control"
+                      as="textarea"
+                      name="description"
+                      value={values.description}
+                    />
+                    {errors.description && touched.description ? (
+                      <span className="error text-center">{errors.description}</span>
+                    ) : null}
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="price">Price</label>
+                    <Field
+                      className="form-control"
+                      type="number"
+                      name="price"
+                      value={values.price}
+                    />
+                    {errors.price && touched.price ? (
+                      <span className="error text-center">{errors.price}</span>
+                    ) : null}
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="picture">Picture</label>
+                    <Field className="form-control" type="url" name="picture" value={values.url} />
+                    {errors.picture && touched.picture ? (
+                      <span className="error text-center">{errors.picture}</span>
+                    ) : null}
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="tags">Tags</label>
+                    <Field className="form-control" as="textarea" name="tags" value={values.tags} />
+                    {errors.tags && touched.tags ? (
+                      <span className="error text-center">{errors.tags}</span>
+                    ) : null}
+                  </div>
+                  <div className="form-group">
+                    <button className="btn btn-modal" type="submit">
+                      Create
+                    </button>
+                  </div>
+                </fieldset>
+              </Form>
+            )}
+          </Formik>
         </div>
       </DialogContent>
       <DialogActions>
--- src/sharedComponents/Header/Registration.js
+++ src/sharedComponents/Header/Registration.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'proptypes';
 import * as yup from 'yup';
 import { Formik, Form, Field } from 'formik';
@@ -6,24 +6,15 @@ import { Formik, Form, Field } from 'formik';
 const Registration = props => {
   const { addUser, onRequestClose } = props;
 
-  const [newUser, setNewUser] = useState({
-    fname: '',
-    lname: '',
-    mail: '',
-    pass: '',
-    role: 'user',
-    deleteRequest: false,
-  });
-
-  const [invalidForm, setInvalidForm] = useState(false);
-
   const schema = yup.object().shape({
     fname: yup
       .string()
+      .trim()
       .min(3, 'First name must be longer')
-      .required(),
+      .required('Required'),
     lname: yup
       .string()
+      .trim()
       .min(3, 'Last name must be longer')
       .required('Required'),
     mail: yup
@@ -32,25 +23,15 @@ const Registration = props => {
       .required('Required'),
     pass: yup
       .string()
+      .trim()
       .min(6, 'Password must be longer')
       .required('Required'),
   });
 
-  const handleAddUser = e => {
-    const { name, value } = e.target;
-    setNewUser({ ...newUser, [name]: value });
-  };
-
-  const add = e => {
-    e.preventDefault();
-    schema.isValid(newUser).then(valid => {
-      if (valid) {
-        addUser(newUser);
-        onRequestClose();
-      } else {
-        setInvalidForm(true);
-      }
-    });
+  const add = values => {
+    const newUser = { ...values, role: 'user', deleteRequest: false };
+    addUser(newUser);
+    onRequestClose();
   };
 
   return (
@@ -59,67 +40,43 @@ const Registration = props => {
         <Formik
           initialValues={{ fname: '', lname: '', mail: '', pass: '' }}
           validationSchema={schema}
+          onSubmit={add}
         >
-          {({ errors, touched }) => (
+          {({ values, errors, touched }) => (
             <Form>
               <fieldset>
                 <div className="form-group">
                   <label htmlFor="fname">First name:</label>
-                  <Field
-                    type="text"
-                    className="form-control"
-                    name="fname"
-                    value={newUser.fname}
-                    onChange={handleAddUser}
-                  />
+                  <Field type="text" className="form-control" name="fname" value={values.fname} />
                   {errors.fname && touched.fname ? (
                     <span className="error text-center">{errors.fname}</span>
                   ) : null}
                 </div>
                 <div className="form-group">
                   <label htmlFor="lname">Last name:</label>
-                  <Field
-                    type="text"
-                    className="form-control"
-                    name="lname"
-                    value={newUser.lname}
-                    onChange={handleAddUser}
-                  />
+                  <Field type="text" className="form-control" name="lname" value={values.lname} />
                   {errors.lname && touched.lname ? (
                     <span className="error text-center">{errors.lname}</span>
                   ) : null}
                 </div>
                 <div className="form-group">
                   <label htmlFor="mail">E-mail:</label>
-                  <Field
-                    type="email"
-                    className="form-control"
-                    name="mail"
-                    value={newUser.mail}
-                    onChange={handleAddUser}
-                  />
+                  <Field type="email" className="form-control" name="mail" value={values.mail} />
                   {errors.mail && touched.mail ? (
                     <span className="error text-center">{errors.mail}</span>
                   ) : null}
                 </div>
                 <div className="form-group">
                   <label htmlFor="pass">Password:</label>
-                  <Field
-                    type="password"
-                    className="form-control"
-                    name="pass"
-                    value={newUser.pass}
-                    onChange={handleAddUser}
-                  />
+                  <Field type="password" className="form-control" name="pass" value={values.pass} />
                   {errors.pass && touched.pass ? (
                     <span className="error text-center">{errors.pass}</span>
                   ) : null}
                 </div>
                 <div className="text-center mt-2">
-                  <button type="submit" onClick={add} className="btn btn-modal">
+                  <button type="submit" className="btn btn-modal">
                     Create account
                   </button>
-                  {invalidForm ? <span className="error">Invalid form!</span> : null}
                 </div>
               </fieldset>
             </Form>
